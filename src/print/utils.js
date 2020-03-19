39c4966c6eb8bb27209a4444e905e1d6f66685ed
hangaoke1 <188869009@qq.com>
2020-03-19 14:33:23 +0800

    feat: update

---
+++ src/print/utils.js
@@ -0,0 +1,40 @@
+/* eslint-disable */
+/***
+ * 
+ * 获取请求的UUID，指定长度和进制,如 
+ * getUUID(8, 2)   //"01001010" 8 character (base=2)
+ * getUUID(8, 10) // "47473046" 8 character ID (base=10)
+ * getUUID(8, 16) // "098F4D35"。 8 character ID (base=16)
+ *   
+ */
+export function getUUID (len, radix) {
+  var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split('')
+  var uuid = [],
+    i
+  radix = radix || chars.length
+  if (len) {
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)]
+  } else {
+    var r
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-'
+    uuid[14] = '4'
+    for (i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        r = 0 | (Math.random() * 16)
+        uuid[i] = chars[i == 19 ? (r & 0x3) | 0x8 : r]
+      }
+    }
+  }
+  return uuid.join('')
+}
+
+/***
+ * 构造request对象
+ */
+export function getRequestObject (cmd) {
+  var request = new Object()
+  request.requestID = getUUID(8, 16)
+  request.version = '1.0'
+  request.cmd = cmd
+  return request
+}
