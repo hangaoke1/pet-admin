06a6f70b96045a606c98b4b39772886a0758fc96
hangaoke1 <188869009@qq.com>
2020-10-26 18:08:42 +0800

    feat: update

--- src/views/cashier/index.vue
+++ src/views/cashier/index.vue
@@ -283,13 +283,14 @@ export default {
       // 商品信息
       const skuStr = orderInfo.skuJsonString
         .map(sku => {
-          return `<div style="display: flex;margin-top: 2mm;">
-                  <div style="width: 30mm;font-size: 10pt;">${sku.name}</div>
-                  <div style="width: 21mm;text-align:center;font-size: 10pt;">${sku.quantity}</div>
-                  <div style="width: 21mm;text-align:center;font-size: 10pt;">${sku.price}</div>
+          return `
+                <div style="display: flex;margin-top: 2mm;">
+                  <div style="width: 30mm;font-size: 9pt;">${sku.name}</div>
+                  <div style="width: 21mm;text-align:center;font-size: 9pt;">${sku.quantity}</div>
+                  <div style="width: 21mm;text-align:center;font-size: 9pt;">${sku.price.toFixed(2)}</div>
                 </div>`
         })
-        .join('')
+        .join('\n')
       const LODOP = getLodop()
       LODOP.SET_PRINT_STYLE('FontSize', 8)
       LODOP.ADD_PRINT_HTM(
@@ -297,26 +298,28 @@ export default {
         0,
         '100%',
         '100%',
-        `
-        <div>
+        `<div>
           <h1  style="font-size: 16pt;text-align: center">${storeInfo.storeName}</h1>
           <div style="margin-bottom: 2mm;font-size: 10pt;">订单号：${orderId}</div>
           <div style="font-size: 10pt;">收银员：${storeInfo.storeName}</div>
           <div style="padding: 2mm 0;font-size: 10pt;">= = = = = = = = = = = = = = = = = = = = = = =</div>
           <div style="display: flex">
-            <div style="width: 30mm;font-size: 10pt;">商品名称</div>
-            <div style="width: 21mm;text-align:center;font-size: 10pt;">数量</div>
-            <div style="width: 21mm;text-align:center;font-size: 10pt;">单价</div>
+            <div style="width: 30mm;font-size: 9pt;">商品名称</div>
+            <div style="width: 21mm;text-align:center;font-size: 9pt;">数量</div>
+            <div style="width: 21mm;text-align:center;font-size: 9pt;">单价</div>
           </div>
           ${skuStr}
           <div style="padding: 2mm 0;font-size: 10pt;">= = = = = = = = = = = = = = = = = = = = = = =</div>
-          <div style="margin-bottom: 2mm;font-size: 10pt;">店铺地址：${storeInfo.address}</div>
+          <div style="margin-bottom: 2mm;font-size: 10pt;">收款金额：${orderInfo.paidFee.toFixed(2)}元</div>
+          <div style="margin-bottom: 2mm;font-size: 10pt;">店铺地址：${storeInfo.address || '浙江省杭州市滨江区滨盛路1893号'}</div>
           <div style="margin-bottom: 2mm;font-size: 10pt;">联系方式：${storeInfo.mobile}</div>
           <div style="margin-bottom: 2mm;font-size: 10pt;">下单时间：${date}</div>
-        </div>
-      `
+          <img border="0" src="https://petcdd-1302550149.cos.ap-shanghai.myqcloud.com/8fa12292-f058-4c41-9be4-fa687bad38c2_gh_fe6998e56d68_258.jpg" style="width: 40mm;height:auto;" /> 
+        </div>`
       )
-      LODOP.PREVIEW()
+      // LODOP.ADD_PRINT_BARCODE(200, 20, 100, 100, 'QRCode', '1234567890版本7的最大值是122个字符123123');
+      LODOP.SET_PRINT_PAGESIZE(3, 720, 45, '');
+      LODOP.PRINT()
     },
     // 下单
     doConfirm() {
